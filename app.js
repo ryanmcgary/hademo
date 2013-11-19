3deb0eda60125c84a97ebc4b975649dd11716147
Ryan McGary <rmcgary>
2013-11-19 14:22:41 -0600

    update to hoist

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var foo = 1;
 function bar() {
-	if (!foo) {
-		var foo = 10;
-	}
-	console.log(foo);
+  if (!foo) {
+    var foo = 10;
+  }
+  console.log(foo);
 }
 bar();
 
 
 var a = 1;
 function b() {
-	a = 10;
-	return;
-	function a() {}
+  a = 10;
+  return;
+  function a() {}
 }
 b();
 console.log(a);
